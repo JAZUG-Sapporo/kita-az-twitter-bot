ad334b2b80e39c3731527d09626a3d8ae27b2e07
jsakamoto <junichi.jp@gmail.com>
2015-04-07 22:12:43 +0900

    JavaScriptのBundle仮想フォルダ位置を変更 - 相対指定されている.mapファイルのパスが、実在しない"~/bundles" を指してしまうため

--- myBot/App_Start/BundleConfig.cs
+++ myBot/App_Start/BundleConfig.cs
@@ -8,13 +8,13 @@ namespace myBot
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+            bundles.Add(new ScriptBundle("~/Scripts/js").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/materialize.js",
                         "~/Scripts/angular.js",
                         "~/Scripts/mybot-common.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            bundles.Add(new ScriptBundle("~/Scripts/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
