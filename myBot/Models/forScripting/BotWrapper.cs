fe58212ede72db4bb21923fcca98b2888dd680a5
jsakamoto <junichi.jp@gmail.com>
2016-03-30 21:44:05 +0900

    Message.IsArchived がスクリプト中で変更できてしまうのを回避するため、スクリプト公開用のラッパーをはさむよう変更

---
+++ myBot/Models/forScripting/BotWrapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace myBot.Models.forScripting
+{
+    /// <summary>
+    /// 拡張スクリプト中でアクセス可能なメンバを制限するための、Bot 型に対するラッパーです。
+    /// </summary>
+    public class BotWrapper
+    {
+        internal Bot Object { get; private set; }
+
+        public MessageWrapper[] Messages { get; private set; }
+
+        public MessageWrapper MessageToNextTweet
+        {
+            get { return this.Messages.FirstOrDefault(msg => msg.Object == this.Object.MessageToNextTweet); }
+            set { this.Object.MessageToNextTweet = value?.Object; }
+        }
+
+        public BotWrapper(Bot bot)
+        {
+            this.Object = bot;
+            this.Messages = bot
+                .Messages
+                .Select(msg => new MessageWrapper(msg))
+                .OrderBy(msg => msg.Order)
+                .ToArray();
+        }
+
+        public CoreTweet.Status Tweet(string text) => this.Object.Tweet(text);
+
+        public void ArchiveMessage(MessageWrapper message) => this.Object.ArchiveMessage(message.Object);
+
+        public void RestoreMessage(MessageWrapper message) => this.Object.RestoreMessage(message.Object);
+    }
+}
---
+++ myBot/Models/forScripting/MessageWrapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace myBot.Models.forScripting
+{
+    /// <summary>
+    /// 拡張スクリプト中でアクセス可能なメンバを制限するための、Message 型に対するラッパーです。
+    /// </summary>
+    public class MessageWrapper
+    {
+        internal Message Object { get; private set; }
+
+        internal int MessageID => Object.MessageID;
+
+        public string Text
+        {
+            get { return this.Object.Text; }
+            set { this.Object.Text = value; }
+        }
+
+        public int Order
+        {
+            get { return this.Object.Order; }
+            set { this.Object.Order = value; }
+        }
+
+        public bool IsArchived => this.Object.IsArchived;
+
+        public MessageWrapper(Message message)
+        {
+            this.Object = message;
+        }
+    }
+}
